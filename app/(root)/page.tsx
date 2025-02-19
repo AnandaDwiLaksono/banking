26624970cd1bb1378b585efa33f2530cf2c9026f
AnandaDwiLaksono <anandad182@gmail.com>
2025-02-19 21:18:25 +0700

    feat: add MyBanks, Transfer, and TransactionHistory pages with corresponding components; update Home and layout for new sidebar and user details

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,13 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Nanda' }
+  const loggedIn = {
+    firstName: 'Ananda Dwi',
+    lastName: 'Laksono',
+    email: 'anandad182@gmail.com'
+  }
 
   return (
     <section className='home'>
@@ -21,7 +26,15 @@ const Home = () => {
             totalCurrentBalance={1250.35}
           />
         </header>
+
+        RECENT TRANSACTIONS
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+      />
     </section>
   )
 }
