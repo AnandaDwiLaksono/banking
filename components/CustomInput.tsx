879f9d104bcdc54d3b01bacb3f9903d13ef42530
AnandaDwiLaksono <anandad182@gmail.com>
2025-02-24 21:48:09 +0700

    feat: enhance authentication forms with custom input components and validation schema; update sign-in and sign-up pages

---
+++ components/CustomInput.tsx
@@ -0,0 +1,42 @@
+import { Control, FieldPath } from 'react-hook-form'
+import { z } from 'zod'
+
+import { FormControl, FormField, FormLabel, FormMessage } from './ui/form'
+import { Input } from './ui/input'
+import { authFormSchema } from '@/lib/utils'
+
+const formSchema = authFormSchema('sign-up')
+
+interface CustomInputProps {
+  control: Control<z.infer<typeof formSchema>>
+  name: FieldPath<z.infer<typeof formSchema>>
+  label: string
+  placeholder: string
+}
+
+const CustomInput = ({ control, name, label, placeholder }: CustomInputProps) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <div className='form-item'>
+          <FormLabel htmlFor="form-label">{label}</FormLabel>
+          <div className='flex flex-col w-full'>
+            <FormControl>
+              <Input
+                {...field}
+                type={name === 'password' ? 'password' : 'text'}
+                placeholder={placeholder}
+                className='input-class'
+              />
+            </FormControl>
+            <FormMessage className='form-message mt-2' />
+          </div>
+        </div>
+      )}
+    />
+  )
+}
+
+export default CustomInput
